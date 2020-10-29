40f3ee8c6dfe4a00fbb8f22f8565eba0229aa4bb
hoyunchoi <hoyun1009@naver.com>
2020-10-29 20:36:27 +0900

    main update

--- main-generate.cpp
+++ main-generate.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <chrono>
 
 #include "generate.hpp"
@@ -18,35 +18,21 @@ int main(int argc, char *argv[]){
 
     //* Determine which observables to calculate
     std::vector<bool> observables(15);
-    observables[0] = true;      //! Order Parameter
-    observables[1] = true;      //! Mean Cluster Size
-    observables[2] = true;      //! Second Giant
-    observables[3] = true;      //! Inter Event Time
-    observables[4] = true;      //! Delta Acceptance
-    observables[5] = true;      //! Order Parameter Distribution
+    observables[0] = false;      //! Order Parameter
+    observables[1] = false;      //! Mean Cluster Size
+    observables[2] = false;      //! Second Giant
+    observables[3] = false;      //! Inter Event Time
+    observables[4] = false;      //! Delta Acceptance
+    observables[5] = false;      //! Order Parameter Distribution
     observables[6] = true;      //! Cluster Size Distribution
-    observables[7] = true;      //! Age Distribution
-    observables[8] = true;      //! Inter Event Time Distribution
-    observables[9] = true;      //! Delta Upper Bound Distribution
-    observables[10] = true;     //! Delta Acceptance Distribution
-    observables[11] = true;     //! Inter Event Time vs Delta Acceptance
-    observables[12] = true;     //! Upper Bound vs Delta Acceptance
-    observables[13] = true;     //! Delta Upper Bound vs Delta Acceptance
+    observables[7] = false;      //! Age Distribution
+    observables[8] = false;      //! Inter Event Time Distribution
+    observables[9] = false;      //! Delta Upper Bound Distribution
+    observables[10] = false;     //! Delta Acceptance Distribution
+    observables[11] = false;     //! Inter Event Time vs Delta Acceptance
+    observables[12] = false;     //! Upper Bound vs Delta Acceptance
+    observables[13] = false;     //! Delta Upper Bound vs Delta Acceptance
     observables[14] = false;    //! Dynamics
-
-    //* run mBFW
-    auto start=std::chrono::system_clock::now();
-    mBFW::generate::setParameters(networkSize, ensembleSize, acceptanceThreshold, precision, coreNum, randomEngineSeed, observables);
-    mBFW::generate::run();
-    std::chrono::duration<double> sec=std::chrono::system_clock::now()-start;
-    FILE* log = fopen("log.txt", "a");
-    fprintf(log, " %.6fs for N=%.1e, g=%.1f, ensemble=%d-%d at %s\n", sec.count(),(double)networkSize, acceptanceThreshold, ensembleSize, coreNum, machine.c_str());
-
-    //* save parameters
-    start = std::chrono::system_clock::now();
-    mBFW::generate::save();
-    sec=std::chrono::system_clock::now()-start;
-    fprintf(log, " %0.6fs for saving\n", sec.count());
 
     //* warning for random engine seed
     if (randomEngineSeed != -1){
@@ -59,5 +45,20 @@ int main(int argc, char *argv[]){
             fprintf(warn, "At N=%.1e, g=%.1f, e=%d, random engine seed is given", (double)networkSize, acceptanceThreshold, ensembleSize);
         }
     }
+
+    //* run mBFW
+    auto start=std::chrono::system_clock::now();
+    mBFW::generate::setParameters(networkSize, ensembleSize, acceptanceThreshold, precision, coreNum, randomEngineSeed, observables);
+    mBFW::generate::run();
+    std::chrono::duration<double> sec=std::chrono::system_clock::now()-start;
+    FILE* log = fopen("log.txt", "a");
+    fprintf(log, " %.6fs for N=%.1e, g=%.1f, ensemble=%d-%d at %s\n", sec.count(),(double)networkSize, acceptanceThreshold, ensembleSize, coreNum, machine.c_str());
+
+    //* save parameters
+    // start = std::chrono::system_clock::now();
+    mBFW::generate::save();
+    // sec=std::chrono::system_clock::now()-start;
+    // fprintf(log, " %0.6fs for saving\n", sec.count());
+
     return 0;
 }
